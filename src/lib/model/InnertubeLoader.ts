a899bea6107e607f942bc4d5442719c6ce5dbc63
patrickkfkan <patrickkfkan@gmail.com>
2024-12-12 15:35:04 +0800

    Lint

--- src/lib/model/InnertubeLoader.ts
+++ src/lib/model/InnertubeLoader.ts
@@ -1,5 +1,5 @@
 import yt2 from '../YouTube2Context';
-import Innertube, { YTNodes } from 'volumio-youtubei.js';
+import Innertube from 'volumio-youtubei.js';
 import BG, { type BgConfig } from 'bgutils-js';
 import { JSDOM } from 'jsdom';
 import { getAccountInitialInfo } from './AccountModelHelper';
