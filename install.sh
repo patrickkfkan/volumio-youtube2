1384bf06a2390fd570b571193fc8137724dfe8ad
patrickkfkan <patrickkfkan@gmail.com>
2025-11-01 16:18:28 +0800

    Fix Deno install path

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ ARCH="$(dpkg --print-architecture)"
 # Deno only supports x64 (amd64) and aarch64 (arm64)
 if [ "${ARCH}" = "amd64" ] || [ "${ARCH}" = "arm64" ]; then
     echo "Installing Deno..."
-    pushd /data/plugins/music_service/ytmusic > /dev/null
+    pushd /data/plugins/music_service/youtube2 > /dev/null
     npm i --omit=dev deno@"^2.5.4"
     popd > /dev/null
 else
